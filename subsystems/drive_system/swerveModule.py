4dd43c674d6cc744a5d9ded2151dba1e3c85b53d
entityJY <jomathya07@gmail.com>
2025-01-09 23:24:21 -0800

    Added comments for swerve module class

--- subsystems/drive_system/swerveModule.py
+++ subsystems/drive_system/swerveModule.py
@@ -9,7 +9,23 @@ from wpimath.units import volts
 from wpilib import RobotController
 
 class SwerveModule:
+    r"""
+    Class for controlling a single swerve module
     
+    **Parameters**:
+        `drivingCANId`: The CAN ID of the driving motor
+        'turningCANId': The CAN ID of the turning motor
+        'encoderNum': The CAN ID of the encoder
+        'reversedDrive': Whether the driving motor is reversed
+        'reversedSteer': Whether the turning motor is reversed
+    
+    Methods:
+    - `getCurrentRotation` - Get current rotation of the module
+    - `resetEncoders` - Resets the rotation and driving encoders to 0
+    - `getState` - Get current state of the module
+    - `getPosition` - Get current position of the module
+    - `setDesiredState` - Sets the state of the module
+    """
     def __init__(self, drivingCANId: int, turningCANId: int, encoderNum: int, reversedDrive: bool, reversedSteer: bool) -> None:
         
         # set up driving motor and encoder
@@ -35,6 +51,11 @@ class SwerveModule:
         self.turningPIDController.enableContinuousInput(c.turnEncoderMin, c.turnEncoderMax)
                 
     def log(self, sys_id_routine: SysIdRoutineLog) -> None:
+        """_summary_
+
+        Args:
+            sys_id_routine (SysIdRoutineLog): _description_
+        """
         sys_id_routine.motor("drive-motor").voltage(
             self.drivingSparkMax.get() * RobotController.getBatteryVoltage()
         ).position(self.getPosition().distance).velocity(
@@ -42,18 +63,23 @@ class SwerveModule:
         )
         
     def getCurrentRotation(self) -> Rotation2d:
-        """
-        Get current rotation of the module
+        """Gets the current rotation of the module
+
+        Returns:
+            Rotation2d: Rotation of the module
         """
         return Rotation2d.fromRotations(self.turningEncoder.get_absolute_position().value_as_double)
     
-    def resetEncoders(self):
+    def resetEncoders(self) -> None:
+        """Resets the rotation and driving encoders to 0"""
         self.drivingEncoder.setPosition(0.0)
         self.turningEncoder.set_position(0.0)
                 
     def getState(self) -> SwerveModuleState:
-        """
-        Get current state of the module
+        """Get current state of the module
+        
+        Returns:
+            SwerveModuleState: The current state of the module
         """
         return SwerveModuleState(self.drivingEncoder.getVelocity(), self.getCurrentRotation())
         
@@ -61,15 +87,16 @@ class SwerveModule:
         """
         Get current position of the module
         
-        :return: The current position of the module
+        Returns:
+            SwerveModulePosition: The current position of the module
         """
         return SwerveModulePosition(self.drivingEncoder.getPosition(),self.getCurrentRotation())
 
     def setDesiredState(self, desiredState: SwerveModuleState) -> None:
-        """
-        Sets the state of the module
-        
-        :param desiredState: The desired state of the module
+        """Sets the state of the module
+
+        Args:
+            `desiredState` (SwerveModuleState): The desired state of the module
         """
         desiredState.optimize(self.getCurrentRotation())
         
